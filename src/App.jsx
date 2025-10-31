959c15b7cfcf3e39dc0847eae49c642cc9797075
Chiboy Cristian <chiboycristiansibarani@gmail.com>
2025-10-31 23:20:57 +0700

    Initial commit: Setup Vit, React, and Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-linear-to-r from-cyan-950 via-pink-950 to-slate-950 text-white">
+      <h1 className="text-4xl font-bold text-blue-cyan  max-w-3xl text-center leading-relaxed">
+        Portal SSO Pemerintahan - Ayo Rek Bantai! Makan Sayang
+      </h1>
+    </div>
+  );
+}
+
+export default App;
